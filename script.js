92c03c833b43e5e45e47955070b35a5c327c65d2
priyanshu ahir <priyanshuahir777@gmail.com>
2026-07-31 17:25:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,5 +14,5 @@ function firstNonRepeatedChar(str) {
 	}
 	return null;
 }
-// const input = prompt("Enter a string");
+const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
