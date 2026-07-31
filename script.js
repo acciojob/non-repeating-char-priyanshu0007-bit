230a0f406d97cd62ad7e5291a6e5de64b5f3ce2e
priyanshu ahir <priyanshuahir777@gmail.com>
2026-07-31 17:22:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-	for(let i=0i<str.length;i++){
+	for(let i=0;i<str.length;i++){
 		let count =0;
 		
 		for(let j=0;j<str.length;j++){
